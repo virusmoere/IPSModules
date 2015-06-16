5bfab93500c85cbad6669a7191527dca2c3bdd0c
virusmoere <virusm03r3@gmail.com>
2015-06-16 14:24:32 +0200

    initial

---
+++ LW12/LW12_5577.php
@@ -0,0 +1,135 @@
+<?
+
+class LW12_5577 {
+	private $IP = "";
+	private $Port = 5577;
+	
+	public function __construct( $IP, $Port )
+	{
+		$this->IP = $IP;
+		$this->Port = $Port;
+		
+		$this->offset = 36;
+		
+		//Statusinformation
+		$this->power = false;
+		$this->mode = 1;
+		$this->modestate = false;
+		$this->speed = 255;
+		$this->color = 000000;
+	}
+	
+	public function PowerOn()
+	{
+		$command = "cc2333";
+		
+		$this->sendPacket($command, 0);
+	}
+	
+	public function PowerOff()
+	{
+		$command = "cc2433";
+		
+		$this->sendPacket($command, 0);
+	}
+	
+	public function Run()
+	{
+		$command = "cc2133";
+		
+		$this->sendPacket($command, 0);
+	}
+	
+	public function Stop()
+	{
+		$command = "cc2233";
+		
+		$this->sendPacket($command, 0);
+	}
+	
+	public function GetStatus()
+	{
+		$command = "ef0177";
+		
+		// 01: Init (0x66)
+		// 02: Init (0x01)
+		// 03: Off (0x24) / On (0x23)
+		// 04: Mode (0x25 - 0x38)
+		// 05: Running (0x21) / Stopped (0x20)
+		// 06: Speed  (1/10th seconds?) (0x00 - 0xff)
+		// 07: Red (0x00 - 0xff)
+		// 08: Green (0x00 - 0xff)
+		// 09: Blue (0x00 - 0xff)
+		// 10: User Memory used (0xFF) / not used (0x51)
+		// 11: Termination (0x99)
+		
+		$status = $this->sendPacket($command, 12);
+		$status = bin2hex($status);
+		$status = strtoupper($status);
+		$status = str_split($status, 2);
+		
+		
+		switch($status[2])
+		{
+			case '23': 
+				$this->power = true;
+				break;
+			default:
+				$this->power = false;
+		}			
+		
+		$this->mode = hexdec($status[3]) - $this->offset;
+		
+		switch($status[4])
+		{
+			case '21': 
+				$this->modestate = true;
+				break;
+			default:
+				$this->modestate = false;
+		}		
+		
+		$this->speed = intval((abs(hexdec($status[5]) - 32)/31)*100);
+		
+		$this->color = (hexdec($status[6]) * 256 * 256) + (hexdec($status[7]) * 256) + hexdec($status[8]);
+	}
+	
+	public function SetColorDec($decrgb)
+	{
+		$r = floor($decrgb/65536);
+		$g = floor(($decrgb-($r*65536))/256);
+		$b = $decrgb-($g*256)-($r*65536);
+		$hexrgb = str_pad(dechex($r),2,0,STR_PAD_LEFT) . str_pad(dechex($g),2,0,STR_PAD_LEFT) . str_pad(dechex($b),2,0,STR_PAD_LEFT);
+		while (strlen($hexrgb) < 6) {
+			$hexrgb = '0'.$hexrgb;
+		}
+
+		$command = '56' . $hexrgb . 'aa';
+
+		$this->sendPacket($command, 0);
+	}
+	
+	public function SetMode($mode, $speed)
+	{
+		$speed = intval(abs(($speed/100) * 31 - 32));
+		$command = 'bb' . dechex($mode + $this->offset) . str_pad(dechex($speed),2,0,STR_PAD_LEFT) . '44';
+
+		$this->sendPacket($command, 0);
+	}
+	
+	private function sendPacket( $command, $return )
+	{
+		$fp = fsockopen($this->IP, $this->Port, $errno, $errstr, 10);
+		if (!$fp)
+		    throw new Exception("Error opening socket: ".$errstr." (".$errno.")");
+		$command = hex2bin($command);
+		fputs ($fp, $command);
+		$ret = 0;
+		if($return > 0)
+			$ret= fgets($fp,$return);
+		fclose($fp);
+		return $ret;
+	}
+}
+
+?>
---
+++ LW12/module.php
@@ -0,0 +1,207 @@
+<?
+
+	class LW12 extends IPSModule
+	{
+		
+		public function __construct($InstanceID)
+		{
+			//Never delete this line!
+			parent::__construct($InstanceID);
+			
+			//These lines are parsed on Symcon Startup or Instance creation
+			//You cannot use variables here. Just static values.
+			$this->RegisterPropertyString("LW12_IP", "");
+			$this->RegisterPropertyInteger("LW12_Port", 5577);
+			$this->RegisterPropertyString("LW12_Type", "");
+			
+			include_once(__DIR__ . "/LW12_5577.php");
+		}
+	
+		public function ApplyChanges()
+		{
+			//Never delete this line!
+			parent::ApplyChanges();
+			
+			$this->RegisterProfileIntegerEx("Mode.LW12", "ArrowRight", "", "", Array(
+				Array(1, "Seven color cross fade", "", -1),
+				Array(2, "Red gradual change", "", -1),
+				Array(3, "Green gradual change", "", -1),
+				Array(4, "Blue gradual change", "", -1),
+				Array(5, "Yellow gradual change", "", -1),
+				Array(6, "Cyan gradual change", "", -1),
+				Array(7, "Purple gradual change", "", -1),
+				Array(8, "White gradual change", "", -1),
+				Array(9, "Red", "", -1),
+				Array(10, "Red blue cross fade", "", -1),
+				Array(11, "Green blue cross fade", "", -1),
+				Array(12, "Seven color strobe flash", "", -1),
+				Array(13, "Red strobe flash", "", -1),
+				Array(14, "Green strobe flash", "", -1),
+				Array(15, "Blue strobe flash", "", -1),
+				Array(16, "Yellow strobe flash", "", -1),
+				Array(17, "Cyan strobe flash", "", -1),
+				Array(18, "Purple strobe flash", "", -1),
+				Array(19, "White strobe flash", "", -1),
+				Array(20, "Seven color jumping change", "", -1),
+			));
+			
+			$this->RegisterVariableBoolean("power", "Status", "~Switch");
+			$this->EnableAction("power");
+			
+			$this->RegisterVariableInteger("mode", "Modus", "Mode.LW12");
+			$this->EnableAction("mode");
+			
+			$this->RegisterVariableInteger("speed", "Geschwindigkeit", "~Intensity.100");
+			$this->EnableAction("speed");
+			
+			$this->RegisterVariableInteger("color", "Farbe", "~HexColor");
+			$this->EnableAction("color");
+		}
+		
+		private function init()
+		{
+			switch($this->ReadPropertyString("LW12_Type"))
+			{
+				case "LEDXXX":
+					$LW12 = new LW12_5577($this->ReadPropertyString("LW12_IP"), $this->ReadPropertyInteger("LW12_Port"));
+					break;
+				case "HX001":
+					throw new Exception("Not implemented");
+					break;
+				default:
+					throw new Exception("Invalid controller type");
+			}
+			return $LW12;
+		}
+		
+		/**
+		* This function will be available automatically after the module is imported with the module control.
+		* Using the custom prefix this function will be callable from PHP and JSON-RPC through:
+		*
+		* LW12_PowerOn($id);
+		*
+		*/
+		
+		public function PowerOn()
+		{
+			$LW12 = $this->init();
+			$LW12->PowerOn();
+		}
+		
+		public function PowerOff()
+		{
+			$LW12 = $this->init();
+			$LW12->PowerOff();
+		}
+		
+		public function Run()
+		{
+			$LW12 = $this->init();
+			$LW12->Run();
+		}
+		
+		public function Stop()
+		{
+			$LW12 = $this->init();
+			$LW12->Stop();
+		}
+		
+		public function SetColorDec($decrgb)
+		{
+			$LW12 = $this->init();
+			$LW12->SetColorDec($decrgb);
+		}
+		
+		public function SetMode($mode)
+		{
+			$LW12 = $this->init();
+			$LW12->SetMode($mode, GetValue($this->GetIDForIdent("speed")));
+		}
+		
+		public function SetSpeed($speed)
+		{
+			$LW12 = $this->init();
+			$LW12->SetMode(GetValue($this->GetIDForIdent("mode")), $speed);
+		}
+		
+		public function GetStatus()
+		{
+			$LW12 = $this->init();
+			$LW12->GetStatus();
+			
+			SetValue($this->GetIDForIdent("power"), $LW12->power);
+			SetValue($this->GetIDForIdent("mode"), $LW12->mode);
+			SetValue($this->GetIDForIdent("speed"), $LW12->speed);
+			SetValue($this->GetIDForIdent("color"), $LW12->color);
+		}
+		
+		public function RequestAction($Ident, $Value)
+		{
+			switch($Ident) {
+				case "power":
+					if($Value)
+					{
+						$this->PowerOn();
+						SetValue($this->GetIDForIdent($Ident), $Value);
+					}
+					else
+					{
+						$this->PowerOff();
+						SetValue($this->GetIDForIdent($Ident), $Value);
+					}
+					break;
+				case "mode": // Mode 1-21
+					if($Value > 0 && $Value < 22)
+					{
+						$this->SetMode($Value);
+						SetValue($this->GetIDForIdent($Ident), $Value);
+					}
+					else
+					{
+						throw new Exception("Invalid mode (only 1-21)");
+					}
+					break;
+				case "speed": // 0-100%
+					$this->SetSpeed($Value);
+					SetValue($this->GetIDForIdent($Ident), $Value);
+					break;
+				case "color": // ~HexColor
+					$this->SetColorDec($Value);
+					SetValue($this->GetIDForIdent($Ident), $Value);
+					break;
+				default:
+					throw new Exception("Invalid ident");
+			}		
+			
+			$this->GetStatus();
+		}
+		
+		//Remove on next Symcon update
+		protected function RegisterProfileInteger($Name, $Icon, $Prefix, $Suffix, $MinValue, $MaxValue, $StepSize) {
+		
+			if(!IPS_VariableProfileExists($Name)) {
+				IPS_CreateVariableProfile($Name, 1);
+			} else {
+				$profile = IPS_GetVariableProfile($Name);
+				if($profile['ProfileType'] != 1)
+					throw new Exception("Variable profile type does not match for profile ".$Name);
+			}
+			
+			IPS_SetVariableProfileIcon($Name, $Icon);
+			IPS_SetVariableProfileText($Name, $Prefix, $Suffix);
+			IPS_SetVariableProfileValues($Name, $MinValue, $MaxValue, $StepSize);
+			
+		}
+		
+		protected function RegisterProfileIntegerEx($Name, $Icon, $Prefix, $Suffix, $Associations) {
+		
+			$this->RegisterProfileInteger($Name, $Icon, $Prefix, $Suffix, $Associations[0][0], $Associations[sizeof($Associations)-1][0], 0);
+		
+			foreach($Associations as $Association) {
+				IPS_SetVariableProfileAssociation($Name, $Association[0], $Association[1], $Association[2], $Association[3]);
+			}
+			
+		}
+	}
+
+?>
